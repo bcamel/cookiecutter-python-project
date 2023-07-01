88b52aff83325e5c3661d0a072ec1c76bb1103bb
bcamel <msiemeri@posteo.net>
2023-07-01 11:03:51 +0200

    Small fixes

---
+++ {{cookiecutter.package_name}}/tests/test_e2e.py
@@ -0,0 +1,14 @@
+import unittest
+
+import {{cookiecutter.package_name}}
+
+
+class EndToEndTestCase(unittest.TestCase):
+    """Version tests"""
+
+    def test_end_to_end(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
